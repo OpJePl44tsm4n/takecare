eb216302e64e268b4cff87934ec341b460a7b2a6
Pim <pim.plaatsman@gmail.com>
2020-05-06 21:28:24 +0200

    fix css

--- site/web/app/themes/takecare-io/inc/row-team.php
+++ site/web/app/themes/takecare-io/inc/row-team.php
@@ -9,7 +9,7 @@
 					$image = wp_get_attachment_image( $image_id, 'thumb', "", array( "class" => "rounded-circle" )  );
 					$name = get_sub_field('name');
 					$job_title = get_sub_field('job_title');
-					$linkedin = get_sub_field('linkedin_url') ? '<a target="_blank" href="'. get_sub_field('linkedin_url') .'" class="linkedin"><i class="fa fa-linkedin"></i></a>' : ''; ?>
+					$linkedin = get_sub_field('linkedin_url') ? '<a target="_blank" href="'. get_sub_field('linkedin_url') .'" class="linkedin"><i class="fab fa-linkedin"></i></a>' : ''; ?>
 
 					<article class="team-member col-xs-1 col-sm-4 col-md-3">
 			            <div class="member-card">
